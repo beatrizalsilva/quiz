595b1949ab4f3d43dfd495139732b66380b109d1
Beatriz <alvesbeatriz148@gmail.com>
2024-02-06 15:51:09 -0300

    finalização da estrutura e da lógica

--- script.js
+++ script.js
@@ -54,10 +54,18 @@ const perguntas = [
 const quiz = document.querySelector("#quiz")
 const template = document.querySelector("template")
 
+// cria um novo conjunto para guardar informações das respostas selecionadas (não repete dado)
+const corretas = new Set()
+// length faz a somatória a partir do 1
+const totalDePerguntas = perguntas.length
+const mostrarTotal = document.querySelector("#acertos span")
+mostrarTotal.textContent = corretas.size + " de " + totalDePerguntas
+
 // laço para pergunta
 for (const item of perguntas) {
     // o método cloneNode vai clonar o template da pergunta
     const quizItem = template.content.cloneNode(true)
+
     // modifica o h3 para que a pergunta saia
     quizItem.querySelector("h3").textContent = item.pergunta
 
@@ -65,8 +73,27 @@ for (const item of perguntas) {
     for(let resposta of item.respostas) {
         // dentro do dl procura o dt, clona templete de resposta
         const dt = quizItem.querySelector("dl dt").cloneNode(true)
+
         // modifica o span para que saia as opções de resposta
         dt.querySelector("span").textContent = resposta
+
+        // faz com que cada input receba uma resposta diferente
+        dt.querySelector("input").setAttribute("name", "pergunta-" + perguntas.indexOf(item))
+        
+        // ajusta o valor (value) do index de cada resposta
+        dt.querySelector("input").value = item.respostas.indexOf(resposta)
+
+        // criar evento para mundar o input
+        dt.querySelector("input").onchange = (event) => {
+            const estaCorreta = event.target.value == item.correta
+            // contabiliza os acertos e se mudar de opnião, tira o ponto de acerto
+            corretas.delete(item)
+            if(estaCorreta) {
+                corretas.add(item)
+            }
+            mostrarTotal.textContent = corretas.size + "de " + totalDePerguntas
+        }
+
         // coloca as opções de resposta na tela
         quizItem.querySelector("dl").appendChild(dt)
     }
